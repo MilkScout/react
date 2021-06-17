2957ff09587b6faf5bf7e6089ec027bd694cdc09
Michael Mannseicher <m.mansi1@gmx.net>
2021-06-18 00:38:02 +0200

    fix: identity map bug

--- src/function/getIdentityMap.ts
+++ src/function/getIdentityMap.ts
@@ -1,18 +1,10 @@
 export type IdPropertyFunction<T> = (element: T) => string;
-export type IdPropertyKey<T> = keyof T;
 
-export const getIdentityMap = <T>(
-  array: Array<T>,
-  idProperty: IdPropertyKey<T> | IdPropertyFunction<T>,
-): Record<string, T> => {
+export const getIdentityMap = <T>(array: Array<T>, idPropertyFun: IdPropertyFunction<T>): Record<string, T> => {
   const resultMap: Record<string, T> = {};
   array.forEach((element) => {
-    if (typeof idProperty === 'function') {
-      const propertyKey = idProperty(element);
-      resultMap[propertyKey] = element;
-    } else {
-      resultMap[idProperty as string] = element;
-    }
+    const propertyKey = idPropertyFun(element);
+    resultMap[propertyKey] = element;
   });
   return resultMap;
 };
