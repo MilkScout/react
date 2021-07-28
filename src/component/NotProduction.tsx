84e311c2ecdce4682e33911b226114544b7a73e3
Michael Mannseicher <m.mansi1@gmx.net>
2021-07-28 10:12:04 +0200

    chore: formatting

--- src/component/NotProduction.tsx
+++ src/component/NotProduction.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react';
-import {when} from "../function";
+import { when } from '../function';
 
 export const NotProduction = ({ children }: PropsWithChildren<unknown>) =>
-    when(process.env.NODE_ENV !== 'production', <>{children}</>, null);
+  when(process.env.NODE_ENV !== 'production', <>{children}</>, null);
