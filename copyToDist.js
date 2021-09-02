59818fa6c0ce7a00a31f81489920787363065ea6
Michael Mannseicher <m.mansi1@gmx.net>
2021-09-02 13:08:06 +0200

    fix: render bug

--- copyToDist.js
+++ copyToDist.js
@@ -14,7 +14,6 @@ const packageJSONKeys = [
     'author',
     'license',
     'peerDependencies',
-    'dependencies',
 ];
 
 const packageJSON = Object.entries(JSON.parse(fs.readFileSync('package.json').toString()))
