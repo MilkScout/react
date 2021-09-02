e3238198b24d7931f64c0b9d1452b359b004524f
Michael Mannseicher <m.mansi1@gmx.net>
2021-09-02 10:55:27 +0200

    fix: copy to dist

--- copyToDist.js
+++ copyToDist.js
@@ -14,6 +14,7 @@ const packageJSONKeys = [
     'author',
     'license',
     'peerDependencies',
+    'dependencies',
 ];
 
 const packageJSON = Object.entries(JSON.parse(fs.readFileSync('package.json').toString()))
